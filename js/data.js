b103eb57dee206b094213e76fee0d009092aac19
FOA-developer <flourishagbanoma@gmail.com>
2026-01-19 16:11:18 +0100

    cretaed a data.js file and added the about box grid info in an arrray that has objects

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const aboutInfo = [
+  {
+    image : 'source' ,
+    header : 'Education',
+    info : 'Providing comprehensive technology training programs that cover coding, design, AI, and more'
+  },
+  {
+    image : 'source' ,
+    header : 'Mentorship',
+    info : 'Connecting students with experienced tech professionals who guide and inspire their journey'
+  },
+  {
+    image : 'source' ,
+    header : 'Opportunities',
+    info : 'Creating pathways to internships, jobs, and entrepreneurship in the technology sector'
+  },
+  {
+    image : 'source' ,
+    header : 'Community',
+    info : 'Building a supportive network of young women in tech across Africa'
+  }
+]
+
